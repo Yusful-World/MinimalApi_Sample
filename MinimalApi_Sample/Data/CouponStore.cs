3ae747ed690a6fe7efed3e779bdf282c451dbece
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-12-22 05:22:10 +0100

    Created end points for GetCoupons list and individual coupon

--- MinimalApi_Sample/Data/CouponStore.cs
+++ MinimalApi_Sample/Data/CouponStore.cs
@@ -6,8 +6,8 @@ namespace MinimalApi_Sample.Data
     {
         public static List<Coupon> couponList = new List<Coupon>
         {
-            new Coupon { Id = 1, Name = "10Off", PercentDiscount = 10, IsActive = true },
-            new Coupon { Id = 2, Name = "20Off", PercentDiscount = 20, IsActive = false }
+            new Coupon { Id = 1, Name = "10-Off", PercentDiscount = 10, IsActive = true },
+            new Coupon { Id = 2, Name = "20-Off", PercentDiscount = 20, IsActive = false }
         };
     }
 }
--- MinimalApi_Sample/Program.cs
+++ MinimalApi_Sample/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Builder;
+using MinimalApi_Sample.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,7 +21,30 @@ app.MapGet("/HelloWorld/{id:int}", (int id) =>
 {
     return Results.Ok("int!!" + id);
 });
-app.MapPost("/HelloWorld1", () => Results.Ok("Hello World! I'm Abdullahi Yusuf. Trying out MapPost"));
+app.MapPost("/HelloWorld1", () => {
+return Results.Ok("Hello World! I'm Abdullahi Yusuf. Trying out MapPost");
+});
+
+
+app.MapGet("/api/coupon", () => {
+    return Results.Ok(CouponStore.couponList);
+});
+
+app.MapGet("/api/coupon/{id:int}", (int id) => {
+    return Results.Ok(CouponStore.couponList.FirstOrDefault(c => c.Id == id));
+});
+
+app.MapPost("/api/coupon/", () => {
+    
+}); 
+
+app.MapPut("/api/coupon/", () => {
+    
+}); 
+
+app.MapDelete("/api/coupon/{id:int}", (int id) => {
+    
+});
 
 app.UseHttpsRedirection();
 
