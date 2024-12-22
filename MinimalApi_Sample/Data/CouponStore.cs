c2c1a307ba64b67e6f036f3ed98b67dd68446ecd
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-12-22 05:02:17 +0100

    Add project files.

---
+++ MinimalApi_Sample/Data/CouponStore.cs
@@ -0,0 +1,13 @@
+﻿using MinimalApi_Sample.Models;
+
+namespace MinimalApi_Sample.Data
+{
+    public static class CouponStore
+    {
+        public static List<Coupon> couponList = new List<Coupon>
+        {
+            new Coupon { Id = 1, Name = "10Off", PercentDiscount = 10, IsActive = true },
+            new Coupon { Id = 2, Name = "20Off", PercentDiscount = 20, IsActive = false }
+        };
+    }
+}
---
+++ MinimalApi_Sample/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalApi_Sample.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PercentDiscount {  get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}
